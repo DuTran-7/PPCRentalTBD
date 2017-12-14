a6c6b4987ece1ac4fd02a50ead987ee92e281b5c
T151081 - Nguyen Dung Minh - K21T01 <minhnguyen100@vanlanguni.vn>
2017-12-14 23:05:38 +0700

    Fillter complete 30%

---
+++ WebPPC.AcceptanceTest/Driver/Fillter/FilterDriver.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebPPC.Models;
+using WebPPC.Controllers;
+using System.Web.Mvc;
+using TechTalk.SpecFlow;
+
+
+
+namespace WebPPC.AcceptanceTest.Driver.Fillter
+{
+    public class FilterDriver
+    {
+        public void InsertData(Table projectCC)
+        {
+            using (team12Entities db  = new team12Entities())
+            {
+                foreach(var item in projectCC)
+                {
+                    
+                }
+            }     
+        }
+    }
+}
---
+++ WebPPC.AcceptanceTest/StepDefinitions/FillterStep.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace WebPPC.AcceptanceTest.StepDefinitions
+{
+    [Binding]
+    public sealed class FillterStep
+    {
+        public readonly Driver.Fillter.FilterDriver _driver;
+
+        [Given(@"co du lieu chinh")]
+        public void GivenCoDuLieuChinh(Table Project)
+        {
+            _driver.InsertData(Project);
+        }
+
+
+        [Given(@"cho phep nguoi dung nhap lieu '(.*)'")]
+        public void GivenChoPhepNguoiDungNhapLieu(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"hien thi danh sach du an vua duoc go")]
+        public void ThenHienThiDanhSachDuAnVuaDuocGo(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
